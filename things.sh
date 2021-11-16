3948bcf6c58a97143686974af3daa753a414f6aa
Thomas Powell <twilliampowell@gmail.com>
2021-11-16 08:19:21 -0600

    Some notes and output tweaks

--- things.sh
+++ things.sh
@@ -5,7 +5,7 @@ function handler() {
     20 scnclr
     30 circle 1,100,100,20
     run
-  " +warp -limitcycles 8000000 -exitscreenshotvicii /tmp/$1.png 2>&1
+  " +warp -limitcycles 8000000 -exitscreenshotvicii /tmp/$1.png 2>&1 >/dev/null
   cd ..
   echo "{\"event_data\":\"$2\", \"status\": \"SUCCESS\", \"data\": \"`base64 /tmp/$1.png`\"}"
 }
