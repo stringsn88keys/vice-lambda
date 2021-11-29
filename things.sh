d1ad24b892c3d3987f1f537c9ad6e7875505cfc4
Thomas Powell <twilliampowell@gmail.com>
2021-11-29 06:38:58 -0600

    Renaming

--- things.sh
+++
@@ -1,9 +0,0 @@
-function handler() {
-  cd vice
-
-  ./x128 -silent -sound -keybuf "`cat maze.bas`" +warp -limitcycles 8000000 -exitscreenshotvicii /tmp/$1.png 2>&1 >/dev/null
-  cd ..
-
-  RESPONSE="{\"isBase64Encoded\": true, \"headers\": {\"Content-type\": \"image/png\", \"content-disposition\":\"attachment; filename=$1.png\"}, \"statusCode\":200, \"body\":\"`base64 /tmp/$1.png`\"}"
-
-}
